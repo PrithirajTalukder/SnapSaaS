26a8536d80dacd2c80dd2a7e489f75e87fc75f31
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2024-03-17 01:53:25 +0600

    Changes to be committed: 	modified:   app/(root)/transformations/add/[type]/page.tsx 	modified:   components/shared/Transformationform.tsx 	new file:   components/ui/input.tsx

--- components/shared/Transformationform.tsx
+++ components/shared/Transformationform.tsx
@@ -1,16 +1,78 @@
 "use client"
-
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string().optional(),
 })
 
 
-const Transformationform = () => {
+
+
+const Transformationform = ({action, data = null}: TransformationFormProps) => {
+
+    const initialValues = data && action === 'Update' ? {
+        title: data?.title,
+        aspectRatio: data?.aspectRatio,
+        color: data?.color,
+        prompt: data?.prompt,
+        publicId: data?.publicId,
+      } : defaultValues
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialValues
+      })
+     
+      
+      function onSubmit(values: z.infer<typeof formSchema>) {
+        
+      
+        console.log(values)
+      }
+    
   return (
-    <div>Transformationform</div>
+    <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input placeholder="shadcn" {...field} />
+            </FormControl>
+            <FormDescription>
+              This is your public display name.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  </Form>
   )
-}
+        }
+
+
+    
 
 export default Transformationform

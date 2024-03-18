d1e3ad1c7f57a5f03bc8b48899fca9e4ca6b7762
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2024-03-19 01:18:21 +0600

    Changes to be committed: 	new file:   components/shared/MediaUploader.tsx 	modified:   components/shared/Transformationform.tsx 	new file:   components/ui/toast.tsx 	new file:   components/ui/toaster.tsx 	new file:   components/ui/use-toast.ts 	modified:   package-lock.json 	modified:   package.json

--- components/shared/Transformationform.tsx
+++ components/shared/Transformationform.tsx
@@ -236,6 +236,26 @@ const Transformationform = ({action, data = null, userId, type, creditBalance, c
       </div>
      )}
 
+     <div className="media-uploader-field">
+      <CustomField
+      control={form.control}
+      name="publicId"
+      className="flex size-full flex-col"
+      render={({ field }) => (
+        <MediaUploader
+       onValueChange = {field.onChange}
+       setImage = {setImage}
+       publicId = {field.value}
+       image = {image}
+       type = {type}
+
+       
+        />
+        )}
+      />
+
+     </div>
+
 
      <div className="flex flex-col gap-4">
 

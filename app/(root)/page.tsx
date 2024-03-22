b02f356b77d192a7ddbdb41db3a4280759fe2bef
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2024-03-23 02:37:16 +0600

    Changes to be committed: 	modified:   app/(root)/page.tsx 	modified:   app/(root)/transformations/[id]/page.tsx 	new file:   components/shared/Collection.tsx 	new file:   components/shared/Search.tsx 	new file:   components/ui/pagination.tsx 	modified:   lib/actions/image.actions.ts 	modified:   next.config.mjs 	modified:   package-lock.json 	modified:   package.json

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,22 @@
+import { Collection } from "@/components/shared/Collection"
 import { navLinks } from "@/constants"
-import { UserButton } from "@clerk/nextjs"
+import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
 
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || '';
 
+  const images = await getAllImages({ page, searchQuery})
 
-const Home = () => {
   return (
-   <>
-   <section className="home">
-
-    <h1 className="home-heading">
-      Unleash your creativity with SnapSaaS
-    </h1>
-
-    <ul className="flex-center w-full gap-20">
+    <>
+      <section className="home">
+        <h1 className="home-heading">
+          Unleash Your Creative Vision with Imaginify
+        </h1>
+        <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
             <Link
               key={link.route}
@@ -28,9 +30,17 @@ const Home = () => {
             </Link>
           ))}
         </ul>
+      </section>
 
-   </section>
-   </>
+      <section className="sm:mt-12">
+        <Collection 
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
+      </section>
+    </>
   )
 }
 
